e069caff8a1f8f51cdf9e1a3679be1babb031a99
jiaolong <jiaolongxu@.gmail.com>
2017-04-06 00:35:19 +0800

    added aux args

--- src/operator/binary_convolution-inl.h
+++ src/operator/binary_convolution-inl.h
@@ -26,7 +26,8 @@ namespace mxnet {
 namespace op {
 
 namespace conv {
-enum ConvolutionOpInputs {kData, kWeight, kBias};
+enum ConvolutionOpInputs {kData, kWeight};
+enum ConvolutionOpAuxiliary {kBinWeight};
 enum ConvolutionOpOutputs {kOut};
 enum ConvolutionOpResource {kTempSpace};
 }
@@ -38,6 +39,7 @@ struct BinaryConvolutionParam : public dmlc::Parameter<BinaryConvolutionParam> {
   TShape pad;
   uint32_t num_group;
   uint32_t num_filter;
+  bool xnor_net;
   dmlc::optional<int> layout;
   DMLC_DECLARE_PARAMETER(BinaryConvolutionParam) {
     DMLC_DECLARE_FIELD(kernel).describe("convolution kernel size: (h, w) or (d, h, w)");
@@ -49,6 +51,8 @@ struct BinaryConvolutionParam : public dmlc::Parameter<BinaryConvolutionParam> {
     .describe("pad for convolution: (h, w) or (d, h, w)");
     DMLC_DECLARE_FIELD(num_group).set_default(1)
     .describe("Number of group partitions.");
+    DMLC_DECLARE_FIELD(xnor_net).set_default(false)
+    .describe("wether or not xnor network.");
     DMLC_DECLARE_FIELD(num_filter).set_range(1, 100000)
     .describe("convolution filter(channel) number");
     DMLC_DECLARE_FIELD(layout)
@@ -79,9 +83,10 @@ class BinaryConvolutionOp : public Operator {
     using namespace mshadow;
     using namespace mshadow::expr;
     
+    CHECK_EQ(aux_args.size(), 1);
     Stream<xpu> *s = ctx.get_stream<xpu>();
-    Tensor<xpu, 4, DType> data = in_data[conv::kData].get<xpu, 4, DType>(s);
-    Tensor<xpu, 4, DType> out = out_data[conv::kOut].get<xpu, 4, DType>(s);
+    Tensor<xpu, 4, DType> data  = in_data[conv::kData].get<xpu, 4, DType>(s);
+    Tensor<xpu, 4, DType> out   = out_data[conv::kOut].get<xpu, 4, DType>(s);
     const size_t nbatch  = data.shape_[0];
     const size_t input_c = data.shape_[1];
     const size_t input_h = data.shape_[2];
@@ -93,9 +98,19 @@ class BinaryConvolutionOp : public Operator {
     const size_t out_w = out.shape_[3];
 
     Shape<2> w_shape = Shape2(f_n, input_c * f_h * f_w);
+    // length of encoded array
+    int len_encode = w_shape[1] / 32 + (w_shape[1] % 32 == 0 ? 0 : 1); 
+
+    Tensor<xpu, 2, DType> w_bin;
+    if (param_.xnor_net) {
+        w_bin = aux_args[conv::kBinWeight].get<xpu, 2, DType>(s);
+        CHECK_EQ(w_bin.shape_[0], f_n) << "binary weight shape[0]";
+        CHECK_EQ(w_bin.shape_[1], len_encode) << "binary weight shape[1]";
+    }
+
     Tensor<xpu, 2, DType> w_real =
         in_data[conv::kWeight].get_with_shape<xpu, 2, DType>(w_shape, s);
-    
+     
     // compute alpha
     size_t num_elements = w_shape[1];
     alpha_.resize(f_n);
@@ -108,35 +123,33 @@ class BinaryConvolutionOp : public Operator {
         
         alpha_[j] /= num_elements;
     }
+     
+    int temp_size = w_shape[1] * out_h * out_w + f_n * out_h * out_w;
+    if (param_.xnor_net)
+        temp_size += len_encode * out_w * out_h;
     
-    // length of encoded array
-    int len_encode = w_shape[1] / 32 + (w_shape[1] % 32 == 0 ? 0 : 1); 
-    Tensor<xpu, 1, unsigned int> workspace1 =
-        ctx.requested[conv::kTempSpace].get_space_typed<xpu, 1, unsigned int>(
-            Shape1(f_n * len_encode + len_encode * out_h * out_w), s);
-    
-    Tensor<xpu, 1, DType> workspace2 =
-        ctx.requested[conv::kTempSpace].get_space_typed<xpu, 1, DType>(
-            Shape1(w_shape[1] * out_h * out_w + f_n * out_h * out_w), s);
-    
-    // storag of encoded weight data
-    Tensor<xpu, 2, unsigned int> w_bin = Tensor<xpu, 2, unsigned int>(
-            workspace1.dptr_, Shape2(f_n, len_encode), s);
-    
-    // storage of encoded binary data
-    Tensor<xpu, 2, unsigned int> temp_col_bin = Tensor<xpu, 2, unsigned int>(
-            workspace1.dptr_ + w_bin.shape_.Size(), Shape2(len_encode, out_h * out_w), s);
-    
-    // encode weight [m, n] -> [m, n/32]
-    binary_op::encode_cols((float*) w_real.dptr_, (unsigned int*) w_bin.dptr_,
-           w_real.size(0), w_real.size(1));
-    
+    Tensor<xpu, 1, DType> workspace 
+        = ctx.requested[conv::kTempSpace].get_space_typed<xpu, 1, DType>(
+                Shape1(temp_size), s);
+
     // storage of image to column data
     Tensor<xpu, 2, DType> temp_col = Tensor<xpu, 2, DType>(
-            workspace2.dptr_, Shape2(w_shape[1], out_h * out_w), s);
+            workspace.dptr_, Shape2(w_shape[1], out_h * out_w), s);
     // storage of dot product result of a path and filter
     Tensor<xpu, 2, DType> temp_dst = Tensor<xpu, 2, DType>(
-            workspace2.dptr_ + temp_col.shape_.Size(), Shape2(f_n, out_h * out_w), s);
+            workspace.dptr_ + temp_col.shape_.Size(), Shape2(f_n, out_h * out_w), s);
+
+    Tensor<xpu, 2, DType> temp_col_bin; 
+    if (param_.xnor_net) { 
+        // storage of encoded binary data
+        temp_col_bin = Tensor<xpu, 2, DType>(
+                workspace.dptr_ + temp_col.shape_.Size() + temp_dst.shape_.Size(),
+                Shape2(len_encode, out_h * out_w), s);
+       
+        // encode weight [m, n] -> [m, n/32]
+        binary_op::encode_cols((float*) w_real.dptr_, (unsigned int*) w_bin.dptr_,
+               w_real.size(0), w_real.size(1));
+    }
     
     const index_t step = 1;
     for (index_t i = 0; i < nbatch; i += step) {
@@ -158,20 +171,23 @@ class BinaryConvolutionOp : public Operator {
                                     param_.dilate[0],
                                     param_.dilate[1]);
       }
-      // encode rows [n, k] -> [n/32, k]
-      binary_op::encode_rows((float*) temp_col.dptr_, (unsigned int*) temp_col_bin.dptr_,
-              temp_col.size(0), temp_col.size(1));
-     
-      binary_op::popcount_xnor_dot((unsigned int*) w_bin.dptr_,
-              (unsigned int*) temp_col_bin.dptr_,
-              w_real.size(0), w_real.size(1), temp_col.size(1),
-              (float*)temp_dst.dptr_, alpha_.data());
-      
-      // dot product
-      //binary_op::bw_dot((float*)w_real.dptr_, (float*)temp_col.dptr_, 
-      //        w_real.size(0), w_real.size(1), temp_col.size(1),
-      //        (float*)temp_dst.dptr_, alpha_.data());
 
+      if (param_.xnor_net) {
+          // encode rows [n, k] -> [n/32, k]
+          binary_op::encode_rows((float*) temp_col.dptr_, (unsigned int*) temp_col_bin.dptr_,
+                  temp_col.size(0), temp_col.size(1));
+
+          binary_op::popcount_xnor_dot((unsigned int*) w_bin.dptr_,
+                  (unsigned int*) temp_col_bin.dptr_,
+                  w_real.size(0), w_real.size(1), temp_col.size(1),
+                  (float*)temp_dst.dptr_, alpha_.data());
+      } else {
+          // dot product
+          binary_op::bw_dot((float*)w_real.dptr_, (float*)temp_col.dptr_, 
+                  w_real.size(0), w_real.size(1), temp_col.size(1),
+                  (float*)temp_dst.dptr_, alpha_.data());
+      }
+      
       out.Slice(i, i + step) = swapaxis<1, 0>(reshape(temp_dst,
                                               mshadow::Shape4(param_.num_filter,
                                                   step,
@@ -214,18 +230,31 @@ class BinaryConvolutionOp : public Operator {
         
     Tensor<xpu, 1, DType> workspace =
         ctx.requested[conv::kTempSpace].get_space_typed<xpu, 1, DType>(
-            Shape1(input_c * f_h * f_w * out_h * out_w + f_n * out_h * out_w), s);
+            Shape1(input_c * f_h * f_w * out_h * out_w + f_n * out_h * out_w + w_shape[1] * w_shape[0]), s);
 
     
     Tensor<xpu, 2, DType> temp_col = Tensor<xpu, 2, DType>(workspace.dptr_, 
             Shape2(input_c * f_h * f_w, out_h * out_w), s);
-    /*
+    
     Tensor<xpu, 2, DType> temp_dst = Tensor<xpu, 2, DType>(
             workspace.dptr_ + temp_col.shape_.Size(),
             Shape2(f_n, out_h * out_w), s);
     
-    //Tensor<xpu, 2, DType> w_real_t;
-    //w_real_t = w_real.T();
+    Tensor<xpu, 2, DType> w_bin = Tensor<xpu, 2, DType>(
+            workspace.dptr_ + temp_col.shape_.Size() + temp_dst.shape_.Size(),
+            Shape2(w_shape[0], w_shape[1]), s);
+
+    for (index_t i = 0; i < w_shape[0]; i++) {
+        for (index_t j = 0; j < w_shape[1]; j++) {
+            if (w_real[i][j] > 0)
+                w_bin[i][j] = alpha_[i];
+            else if (w_real[i][j] < 0)
+                w_bin[i][j] = -1.0 * alpha_[i];
+            else
+                w_bin[i][j] = 0;
+        }
+    }
+
     const index_t step = 1;
     for (index_t i = 0; i < nbatch; i += 1) {
 
@@ -258,6 +287,8 @@ class BinaryConvolutionOp : public Operator {
       //binary_op::bw_dot((float*) w_real_t.dptr_, (float*) temp_dst.dptr_, 
       //        w_real.size(1), w_real.size(0), temp_dst.size(1),
       //        (float*) temp_col.dptr_, alpha_.data());
+      
+      temp_col = dot(w_bin.T(), temp_dst);
       
       if (param_.pad[0] == 0 && param_.pad[1] == 0) {
         Assign(gdata.Slice(i, i + step), req[conv::kData],
@@ -294,7 +325,7 @@ class BinaryConvolutionOp : public Operator {
                 gwmat[j][k] *= ee;
             else
                 gwmat[j][k] *= (alpha_[j] * w_real[j][k] + ee);
-  */
+  
   }
 
  private: 
@@ -342,127 +373,68 @@ class BinaryConvolutionProp : public OperatorProperty {
     using namespace mshadow;
     CHECK_EQ(in_shape->size(), 2) << "Input:[data, weight]";
     out_shape->resize(1, TShape());
+    aux_shape->resize(1, TShape());
     const TShape &dshp = (*in_shape)[conv::kData];
     if (dshp.ndim() ==  0) return false;
-    if (param_.kernel.ndim() == 2) {
-      // 2d conv
-      CHECK_EQ(dshp.ndim(), 4) \
-          << "Input data should be 4D in batch-num_filter-y-x";
-      Shape<4> dshape = ConvertLayout(dshp.get<4>(), param_.layout.value(), kNCHW);
-      Shape<4> wshape = Shape4(param_.num_filter / param_.num_group, dshape[1] / param_.num_group,
-                               param_.kernel[0], param_.kernel[1]);
-      wshape = ConvertLayout(wshape, kNCHW, param_.layout.value());
-      wshape[0] *= param_.num_group;
-      SHAPE_ASSIGN_CHECK(*in_shape, conv::kWeight, wshape);
+    CHECK_EQ(param_.kernel.ndim(), 2);
 
-      const index_t ksize_y = static_cast<index_t>(param_.kernel[0]);
-      const index_t ksize_x = static_cast<index_t>(param_.kernel[1]);
-      CHECK_EQ(dshape[1] % param_.num_group, 0) \
-          << "input num_filter must divide group size";
-      CHECK_EQ(param_.num_filter % param_.num_group, 0) \
-          << "output num_filter must divide group size";
-      CHECK_GT(param_.kernel.Size(), 0) \
-          << "incorrect kernel size: " << param_.kernel;
-      CHECK_GT(param_.stride.Size(), 0) \
-          << "incorrect stride size: " << param_.stride;
-      CHECK_GT(param_.dilate.Size(), 0) \
-          << "incorrect dilate size: " << param_.dilate;
-      Shape<4> oshape;
-      oshape[0] = dshape[0];
-      oshape[1] = param_.num_filter;
-      oshape[2] = (dshape[2] + 2 * param_.pad[0] -
-          (param_.dilate[0] * (ksize_y - 1) + 1)) / param_.stride[0] + 1;
-      oshape[3] = (dshape[3] + 2 * param_.pad[1] -
-          (param_.dilate[1] * (ksize_x - 1) + 1)) / param_.stride[1] + 1;
-      SHAPE_ASSIGN_CHECK(*out_shape, 0, ConvertLayout(oshape, kNCHW, param_.layout.value()));
-      // Perform incomplete shape inference. Fill in the missing values in data shape.
-      // 1) We can always fill in the batch_size.
-      // 2) We can back-calculate the input height/width if the corresponding stride is 1.
-      oshape = ConvertLayout((*out_shape)[0].get<4>(), param_.layout.value(), kNCHW);
-      dshape[0] = oshape[0];
-      if (param_.stride[0] == 1) {
-        dshape[2] = oshape[2] + param_.dilate[0] * (ksize_y - 1) - 2 * param_.pad[0];
-      }
-      if (param_.stride[1] == 1) {
-        dshape[3] = oshape[3] + param_.dilate[1] * (ksize_x - 1) - 2 * param_.pad[1];
-      }
-      SHAPE_ASSIGN_CHECK(*in_shape, conv::kData,
-                          ConvertLayout(dshape, kNCHW, param_.layout.value()));
-      // Check whether the kernel sizes are valid
-      if (dshape[2] != 0) {
-        CHECK_LE(ksize_y, dshape[2] + 2 * param_.pad[0]) << "kernel size exceed input";
-      }
-      if (dshape[3] != 0) {
-        CHECK_LE(ksize_x, dshape[3] + 2 * param_.pad[1]) << "kernel size exceed input";
-      }
-      return true;
-    } else if (param_.kernel.ndim() == 3) {
-      // 3d conv
-      CHECK_EQ(dshp.ndim(), 5) \
-        << "Input data should be 5D in batch-num_filter-depth-y-x";
-      Shape<5> dshape = ConvertLayout(dshp.get<5>(), param_.layout.value(), kNCDHW);
-      Shape<5> wshape = Shape5(param_.num_filter / param_.num_group, dshape[1] / param_.num_group,
-                               param_.kernel[0], param_.kernel[1], param_.kernel[2]);
-      wshape = ConvertLayout(wshape, kNCDHW, param_.layout.value());
-      wshape[0] *= param_.num_group;
-      SHAPE_ASSIGN_CHECK(*in_shape, conv::kWeight, wshape);
+    // 2d conv
+    CHECK_EQ(dshp.ndim(), 4) \
+      << "Input data should be 4D in batch-num_filter-y-x";
+    Shape<4> dshape = ConvertLayout(dshp.get<4>(), param_.layout.value(), kNCHW);
+    Shape<4> wshape = Shape4(param_.num_filter / param_.num_group, dshape[1] / param_.num_group,
+                           param_.kernel[0], param_.kernel[1]);
 
-      const index_t ksize_d = static_cast<index_t>(param_.kernel[0]);
-      const index_t ksize_y = static_cast<index_t>(param_.kernel[1]);
-      const index_t ksize_x = static_cast<index_t>(param_.kernel[2]);
-      CHECK_EQ(dshape[1] % param_.num_group, 0)
-        << "input num_filter must divide group size";
-      CHECK_EQ(param_.num_filter % param_.num_group, 0)
-        << "output num_filter must divide group size";
-      CHECK_GT(param_.kernel.Size(), 0) \
-        << "incorrect kernel size: " << param_.kernel;
-      CHECK_GT(param_.stride.Size(), 0) \
-        << "incorrect stride size: " << param_.stride;
-      CHECK_GT(param_.dilate.Size(), 0) \
-        << "incorrect dilate size: " << param_.dilate;
-      CHECK_EQ(param_.dilate.Size(), 1)
-        << "Dilate is not supported in 3d convolution";
-      Shape<5> oshape;
-      oshape[0] = dshape[0];
-      oshape[1] = param_.num_filter;
-      oshape[2] = (dshape[2] + 2 * param_.pad[0] -
-          (1 * (ksize_d - 1) + 1)) / param_.stride[0] + 1;
-      oshape[3] = (dshape[3] + 2 * param_.pad[1] -
-          (1 * (ksize_y - 1) + 1)) / param_.stride[1] + 1;
-      oshape[4] = (dshape[4] + 2 * param_.pad[2] -
-          (1 * (ksize_x - 1) + 1)) / param_.stride[2] + 1;
-      SHAPE_ASSIGN_CHECK(*out_shape, 0, ConvertLayout(oshape, kNCDHW, param_.layout.value()));
-      // Perform incomplete shape inference. Fill in the missing values in data shape.
-      // 1) We can always fill in the batch_size.
-      // 2) We can back-calculate the input depth/height/width if the corresponding stride is 1.
-      oshape = ConvertLayout((*out_shape)[0].get<5>(), param_.layout.value(), kNCDHW);
-      dshape[0] = oshape[0];
-      if (param_.stride[0] == 1) {
-        dshape[2] = oshape[2] + 1 * (ksize_d - 1) - 2 * param_.pad[0];
-      }
-      if (param_.stride[1] == 1) {
-        dshape[3] = oshape[3] + 1 * (ksize_y - 1) - 2 * param_.pad[1];
-      }
-      if (param_.stride[2] == 1) {
-        dshape[4] = oshape[4] + 1 * (ksize_x - 1) - 2 * param_.pad[2];
-      }
-      SHAPE_ASSIGN_CHECK(*in_shape, conv::kData,
-                          ConvertLayout(dshape, kNCDHW, param_.layout.value()));
-      // Check whether the kernel sizes are valid
-      if (dshape[2] != 0) {
-        CHECK_LE(ksize_d, dshape[2] + 2 * param_.pad[0]) << "kernel size exceed input";
-      }
-      if (dshape[3] != 0) {
-        CHECK_LE(ksize_y, dshape[3] + 2 * param_.pad[1]) << "kernel size exceed input";
-      }
-      if (dshape[4] != 0) {
-        CHECK_LE(ksize_x, dshape[4] + 2 * param_.pad[2]) << "kernel size exceed input";
-      }
-      return true;
-    } else {
-      LOG(FATAL) << "Unknown convolution type";
-      return false;
+    int w_len = wshape[1] * wshape[2] * wshape[3];
+    int len_encode = w_len / 32 + (w_len % 32 == 0 ? 0 : 1); 
+    Shape<2> wbinshape = Shape2(param_.num_filter, len_encode);
+
+    wshape = ConvertLayout(wshape, kNCHW, param_.layout.value());
+    wshape[0] *= param_.num_group;
+    SHAPE_ASSIGN_CHECK(*in_shape, conv::kWeight, wshape);
+    SHAPE_ASSIGN_CHECK(*aux_shape, conv::kBinWeight, wbinshape);
+
+    const index_t ksize_y = static_cast<index_t>(param_.kernel[0]);
+    const index_t ksize_x = static_cast<index_t>(param_.kernel[1]);
+    CHECK_EQ(dshape[1] % param_.num_group, 0) \
+      << "input num_filter must divide group size";
+    CHECK_EQ(param_.num_filter % param_.num_group, 0) \
+      << "output num_filter must divide group size";
+    CHECK_GT(param_.kernel.Size(), 0) \
+      << "incorrect kernel size: " << param_.kernel;
+    CHECK_GT(param_.stride.Size(), 0) \
+      << "incorrect stride size: " << param_.stride;
+    CHECK_GT(param_.dilate.Size(), 0) \
+      << "incorrect dilate size: " << param_.dilate;
+    Shape<4> oshape;
+    oshape[0] = dshape[0];
+    oshape[1] = param_.num_filter;
+    oshape[2] = (dshape[2] + 2 * param_.pad[0] -
+      (param_.dilate[0] * (ksize_y - 1) + 1)) / param_.stride[0] + 1;
+    oshape[3] = (dshape[3] + 2 * param_.pad[1] -
+      (param_.dilate[1] * (ksize_x - 1) + 1)) / param_.stride[1] + 1;
+    SHAPE_ASSIGN_CHECK(*out_shape, 0, ConvertLayout(oshape, kNCHW, param_.layout.value()));
+    // Perform incomplete shape inference. Fill in the missing values in data shape.
+    // 1) We can always fill in the batch_size.
+    // 2) We can back-calculate the input height/width if the corresponding stride is 1.
+    oshape = ConvertLayout((*out_shape)[0].get<4>(), param_.layout.value(), kNCHW);
+    dshape[0] = oshape[0];
+    if (param_.stride[0] == 1) {
+    dshape[2] = oshape[2] + param_.dilate[0] * (ksize_y - 1) - 2 * param_.pad[0];
     }
+    if (param_.stride[1] == 1) {
+    dshape[3] = oshape[3] + param_.dilate[1] * (ksize_x - 1) - 2 * param_.pad[1];
+    }
+    SHAPE_ASSIGN_CHECK(*in_shape, conv::kData,
+                      ConvertLayout(dshape, kNCHW, param_.layout.value()));
+    // Check whether the kernel sizes are valid
+    if (dshape[2] != 0) {
+    CHECK_LE(ksize_y, dshape[2] + 2 * param_.pad[0]) << "kernel size exceed input";
+    }
+    if (dshape[3] != 0) {
+    CHECK_LE(ksize_x, dshape[3] + 2 * param_.pad[1]) << "kernel size exceed input";
+    }
+    return true;
   }
 
   bool InferType(std::vector<int> *in_type,
@@ -479,7 +451,12 @@ class BinaryConvolutionProp : public OperatorProperty {
                                        << "Expected " << dtype << " v.s. given "
                                        << (*in_type)[i] << " at " << ListArguments()[i];
       }
-    }
+    } 
+
+    int n_aux = this->ListAuxiliaryStates().size();
+    aux_type->clear();
+    for (int i = 0; i < n_aux; ++i ) aux_type->push_back(dtype);
+
     out_type->clear();
     out_type->push_back(dtype);
     return true;
@@ -510,6 +487,10 @@ class BinaryConvolutionProp : public OperatorProperty {
   std::vector<ResourceRequest> BackwardResource(
       const std::vector<TShape> &in_shape) const override {
     return {ResourceRequest::kTempSpace};
+  }
+
+  std::vector<std::string> ListAuxiliaryStates() const override {
+    return {"binary_weight"};
   }
 
   Operator* CreateOperator(Context ctx) const override {
